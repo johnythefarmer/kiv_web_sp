740c8435f3cb3314b29a9b953740c65a81d12335
Jan Dvorak <dvorakj50@gmail.com>
2014-11-05 19:46:44 +0100

    pridani twigu

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-  echo $_GET['url'];
+    require_once 'Twig/lib/Twig/Autoloader.php';
+    Twig_Autoloader::register();
+
+	$loader = new Twig_Loader_Filesystem('templates');
+	$twig = new Twig_Environment($loader); // takhle je to bez cache
+
+	$template = $twig->loadTemplate('template1.html');
+    
+    $url = "korenova stranka";
+    if (isset($_GET['url'])) {
+       $url = $_GET['url'];
+    }
+
+    echo $template->render(array('url' => $url, 'title' => 'INDEX'));
 ?>
